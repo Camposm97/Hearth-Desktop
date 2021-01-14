e9e0ed5933de4e22def926d3f048592579d2e1dc
Camposm <camposm1205@gmail.com>
2021-01-13 19:45:49 -0500

    Create MediaImporter.java

---
+++ src/main/java/util/MediaImporter.java
@@ -0,0 +1,9 @@
+package util;
+
+/**
+ * Purpose of this class is that it will prompt the user if they want the program to
+ * scan their files and have the program search for media files that the program supports.
+ */
+public class MediaImporter {
+    
+}
