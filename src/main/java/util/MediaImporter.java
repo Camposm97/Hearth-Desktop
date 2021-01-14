2dec0dbc1e595a36dcd7543f0bfb049b56b6a77d
Camposm <camposm1205@gmail.com>
2021-01-13 20:04:25 -0500

    Update MediaImporter.java
    Writing importMedia method to scan for media files in user's home directory.

--- src/main/java/util/MediaImporter.java
+++ src/main/java/util/MediaImporter.java
@@ -1,9 +1,35 @@
 package util;
 
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Purpose of this class is that it will prompt the user if they want the program to
  * scan their files and have the program search for media files that the program supports.
  */
 public class MediaImporter {
-    
+    /**
+     * Purpose of this main method is to test methods in MediaImporter
+     * I will probably make a tests later on,
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        importMedia();
+    }
+
+    public static void importMedia() {
+        final String HOME = "user.home";
+        String path = System.getProperty(HOME);
+        File fileHome = new File(path);
+        System.out.println(fileHome);
+        List<File> fileList = Arrays.asList(fileHome.listFiles((file, name) -> {
+            return !name.startsWith(".");
+        }));
+
+        fileList.forEach(file -> {
+            System.out.println(file);
+        });
+    }
 }
